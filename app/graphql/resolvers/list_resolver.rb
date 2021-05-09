946af6d90cc320104c962356fae9f32a515d7145
Daniel Polo <od.polo@gmail.com>
2021-05-09 00:05:31 -0500

    feat(controller): ListType to include total pages

--- app/graphql/resolvers/list_resolver.rb
+++ app/graphql/resolvers/list_resolver.rb
@@ -5,14 +5,16 @@ module Resolvers
 
     def resolve(page:, limit:, shuffle: false)
       count = yield.page(page).per(limit).total_count
-      prev_page = page == 1 || (count / limit).ceil < page ? nil : page - 1
-      next_page = (count / limit).ceil <= page ? nil : page + 1
+      pages = (count / limit).ceil
+      prev_page = page == 1 || pages < page ? nil : page - 1
+      next_page = pages <= page ? nil : page + 1
       # FIXME: This isn't random. It just shuffles the page.
       results = shuffle ? yield.page(page).per(limit).shuffle : yield.page(page).per(limit)
       {
         count: count,
         next: next_page,
         prev: prev_page,
+        pages: pages,
         results: results
       }
     end
--- app/graphql/types/list_type.rb
+++ app/graphql/types/list_type.rb
@@ -3,5 +3,6 @@ module Types
     field :count, Int, null: false
     field :next, Int, null: true
     field :prev, Int, null: true
+    field :pages, Int, null: true
   end
 end
